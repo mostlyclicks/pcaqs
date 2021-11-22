8b39ae37acd197f17e1c9d18e1cf95d1539c9d55
mostlyclicks <carlos@mostlyclicks.com>
2021-11-21 21:08:10 -0700

    add AQList

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './components/Header'
+import AQList from './compoents/AQList'
 
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="">
       <Header />
+      <AQList />
     </div>
   );
 }
---
+++ src/components/AQList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AQList = () => {
+  return (
+    <section>
+      <h1>Asked Questions</h1>
+      
+    </section>
+  )
+}
+
+export default AQList
